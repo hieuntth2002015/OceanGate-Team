b92b6713387bf03febc3de8edfc80ddd4950aadf
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2020-09-07 11:18:44 +0700

    product

--- js/main.js
+++ js/main.js
@@ -706,3 +706,16 @@
 })(jQuery);
 
 //# sourceMappingURL=main.js.map
+
+//login//
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container1');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
